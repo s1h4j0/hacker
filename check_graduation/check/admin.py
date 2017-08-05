09f5825bebaece64cb50152ee88db52b92e5d045
SongHyunJae <s1h4j0@skku.edu>
2017-08-05 20:31:58 +0900

    8:31

--- check_graduation/check/admin.py
+++ check_graduation/check/admin.py
@@ -4,14 +4,14 @@ from .models import Category, CategoryDetail, Subject
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'need']
+    list_display = ['id', 'title', 'need']
 
 
 @admin.register(CategoryDetail)
 class CategoryDetailAdmin(admin.ModelAdmin):
-    list_display = ['title', 'need', 'category']
+    list_display = ['id', 'title', 'need', 'category']
 
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ['title', 'credit', 'category_detail']
+    list_display = ['id', 'title', 'credit', 'category_detail']
